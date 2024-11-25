9b6178acd39dd3494a93302018922c00e770645a
JustargonY <super_gleb1996@mail.ru>
2024-11-25 18:01:03 +0100

    added google colab training process + simple lib interface

---
+++ vocalseparatordiploma/prediction.py
@@ -0,0 +1,25 @@
+def load_model():
+    """
+    loads prediction model from file system
+    :return:
+    """
+    pass
+
+
+# should invoke load_model at the start
+
+
+def predict():
+    """
+    calls model.predict()
+    :return:
+    """
+    pass
+
+
+def predict_mocked():
+    """
+    mocked version of _predict, returns random 0-1 vectors
+    :return:
+    """
+    pass
