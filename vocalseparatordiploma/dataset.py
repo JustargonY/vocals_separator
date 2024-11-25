cf8821284c7e91048e69122ba12704bc4dd71e0e
wiktoria <wiktoriakoniecko@gmail.com>
2024-11-25 17:28:17 +0100

    data loading checks

--- vocalseparatordiploma/dataset.py
+++ vocalseparatordiploma/dataset.py
@@ -6,10 +6,41 @@ import os
 import tensorflow as tf
 import numpy as np
 import scipy.io.wavfile as wav
-from scipy.signal import stft
 
 # the audio sample rate, common for both datasets
 SAMPLE_RATE = 44100
+
+
+class UnsupportedWavFileException(Exception):
+    """
+    An exception thrown when a wav file has a sample rate other than 44100Hz or if the file has
+    more or less than 2 channels.
+    """
+
+
+def read_wav_file_to_array(file_path) -> np.ndarray:
+    """
+    Reads a wav file into a numpy array using the scipy.io.wavfile.read function.
+    Asserts that the sample rate is correct and that there are 2 audio channels.
+    Returns just the data array. 
+    """
+    file_path = file_path.numpy().decode("utf-8")
+    # sr is the sample rate of the audio file
+    sample_rate, audio = wav.read(file_path)
+
+    if sample_rate != SAMPLE_RATE:
+        raise UnsupportedWavFileException(
+            "Cannot load file: audio files for processing must have a sample rate of "
+            f"{SAMPLE_RATE}. Sample rate of {file_path} is {sample_rate}."
+            )
+    elif audio.ndim < 2:
+        raise UnsupportedWavFileException("Cannot load file: expected 2 audio channels, found 1.")
+    elif audio.shape[1] != 2:
+        raise UnsupportedWavFileException(
+            f"Cannot load file: expected 2 audio channels, found {audio.shape[1]}.")
+
+    return audio.astype(np.float32)
+
 
 
 def musdb18_get_file_names(dataset_path: str, train_test="train") -> tf.data.Dataset:
@@ -35,36 +66,3 @@ def ccmixter_get_file_names(dataset_path: str) -> tf.data.Dataset:
         list(map(lambda path: os.path.join(path, "mix.wav"), subdirectories)),
         list(map(lambda path: os.path.join(path, "source-02.wav"), subdirectories))
     )
-
-
-def read_wav_file_to_array(file_path) -> np.ndarray:
-    """
-    Reads a wav file into a numpy array using the scipy.io.wavfile.read function.
-    Asserts that the sample rate is correct if assertions are enabled.
-    Returns just the data array. 
-    """
-    file_path = file_path.numpy().decode("utf-8")
-    # sr is the sample rate of the audio file
-    sr, audio = wav.read(file_path)
-    assert sr == SAMPLE_RATE
-
-    return audio.astype(np.float32)
-
-
-def transform_track_stft(signal, sample_rate=SAMPLE_RATE, nperseg=2048, noverlap=1024):
-    """Transforms both channels of a stereo signal using STFT."""
-    params = {"fs": sample_rate, "nperseg": nperseg, "noverlap": noverlap, "window": "hamming"}
-    frequencies, times, transformed_left = stft(signal[:, 0], **params)
-    frequencies, times, transformed_right = stft(signal[:, 1], **params)
-
-    return frequencies, times, transformed_left, transformed_right
-
-
-def get_windows_for_one_chanel(spectrogram, window_size=5):
-    """A generator returning windows of the given width. The edges are padded with zeros."""
-    spectrum_width, length = spectrogram.shape
-    zeros_for_window = np.zeros((window_size // 2, spectrum_width))
-    data_for_windows = np.concatenate([zeros_for_window, spectrogram.T, zeros_for_window])
-
-    for i in range(length):
-        yield data_for_windows[i:i+window_size]
--- vocalseparatordiploma/pretrained.py
+++ vocalseparatordiploma/pretrained.py
@@ -1,8 +1,30 @@
 """This submodule exposes an API for the trained model."""
 
 import numpy as np
-from scipy.signal import istft
-from .dataset import transform_track_stft, get_windows_for_one_chanel
+from scipy.signal import stft, istft
+
+
+SAMPLE_RATE = 44100
+
+
+def get_windows_for_one_chanel(spectrogram, window_size=5):
+    """A generator returning windows of the given width. The edges are padded with zeros."""
+    spectrum_width, length = spectrogram.shape
+    zeros_for_window = np.zeros((window_size // 2, spectrum_width))
+    data_for_windows = np.concatenate([zeros_for_window, spectrogram.T, zeros_for_window])
+
+    for i in range(length):
+        yield data_for_windows[i:i+window_size]
+
+
+def transform_track_stft(signal, sample_rate=SAMPLE_RATE, nperseg=2048, noverlap=1024):
+    """Transforms both channels of a stereo signal using STFT."""
+    params = {"fs": sample_rate, "nperseg": nperseg, "noverlap": noverlap, "window": "hamming"}
+    frequencies, times, transformed_left = stft(signal[:, 0], **params)
+    frequencies, times, transformed_right = stft(signal[:, 1], **params)
+
+    return frequencies, times, transformed_left, transformed_right
+
 
 def predict_for_window(window):
     """Returns a binary mask prediction for a single window."""
