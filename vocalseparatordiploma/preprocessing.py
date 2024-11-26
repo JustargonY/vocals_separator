6272d1431b4738ae5f6e01af986aad1fd2710311
JustargonY <super_gleb1996@mail.ru>
2024-11-26 13:30:49 +0100

    updated colab file, lib interface, added rnn architecture for cpu training

--- vocalseparatordiploma/preprocessing.py
+++ vocalseparatordiploma/preprocessing.py
@@ -1,30 +1,33 @@
-def read_track():
+import numpy as np
+import scipy.io.wavfile as wav
+from scipy.signal import stft
+
+
+def read_track(audio_path: str):
     """
     reads track from file
-    :return:
+    :return: sample rate as int and audio as np.array
     """
-    pass
+    sr, audio = wav.read(audio_path)
+    if audio.dtype == np.int16:
+        audio = audio / 32768.0
+    return sr, audio
 
 
-def compute_stft():
+def compute_stft(audio: np.array, sr: int):
     """
     applies STFT on signal
-    :return:
+    :return: frequencies, times and magnitudes of left and right tracks as np.array
     """
-    pass
+    frequencies, times, Zxx_l = stft(audio[:, 0], fs=sr, nperseg=1024, noverlap=512)
+    _, _, Zxx_r = stft(audio[:, 1], fs=sr, nperseg=1024, noverlap=512)
+
+    return frequencies, times, Zxx_l, Zxx_r
 
 
 def generate_windows():
     """
     generates input data for model
-    :return:
-    """
-    pass
-
-
-def get_ideal_binary_mask():
-    """
-    generates IBM from mix and vocals
     :return:
     """
     pass
