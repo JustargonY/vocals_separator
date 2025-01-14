885f94206555e11adba30ce75496767b566b11d4
wiktoria <wiktoriakoniecko@gmail.com>
2025-01-14 23:29:11 +0100

    use frozendict

--- vocalseparatordiploma/constants.py
+++ vocalseparatordiploma/constants.py
@@ -1,26 +1,10 @@
+from frozendict import frozendict
+
 SAMPLE_RATE = 44100
 SIGNAL_NORMALIZATION_CONSTANT = 32767
 
-class ImmutableDict(dict):
-    """
-    Immutable dictionary class for constants.
-    """
-    def __hash__(self):
-        return id(self)
 
-    def _immutable(self, *args, **kws):
-        raise TypeError('object is immutable')
-
-    __setitem__ = _immutable
-    __delitem__ = _immutable
-    clear       = _immutable
-    update      = _immutable
-    setdefault  = _immutable # type: ignore
-    pop         = _immutable
-    popitem     = _immutable
-
-
-STFT_DEFAULT_PARAMETERS = ImmutableDict({
+STFT_DEFAULT_PARAMETERS = frozendict({
     "fs": SAMPLE_RATE,
     "nperseg": 1024,
     "noverlap": 512,
