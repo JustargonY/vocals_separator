d5e0aa62b2c7511a21bec43ce2e2a4d3c9ee1ea7
wiktoria <wiktoriakoniecko@gmail.com>
2025-01-15 20:43:18 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="vocalseparatordiploma",
+    version="1.0",
+    author="Gleb Grinchik, Wiktoria Koniecko",
+    packages=["vocalseparatordiploma"]
+)
